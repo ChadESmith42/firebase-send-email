7dbdb50fcc0eef7c223f343ea1a6f4a24db283f0
Chad Smith <chad.smith@bluekc.com>
2020-10-03 16:57:10 -0500

    add CORS headers

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,36 @@
 const functions = require('firebase-functions');
 const sendgrid = require('@sendgrid/mail');
-const parser = require('body-parser');
+
 
 exports.contact_us = functions.https.onRequest(async (request, response) => {
 
-    sendgrid.setApiKey(functions.config().sendgrid.apikey);
+    if (request.method === 'OPTIONS') {
+        response.set('Access-Control-Allow-Methods', 'GET');
+        response.set('Access-Control-Allow-Headers', 'Content-Type');
+        response.set('Access-Control-Max-Age', '3600');
+        response.set('Access-Control-Allow-Origin', 'https://codingbychad.com');
+        response.status(204).send('');
+    } else {
+        sendgrid.setApiKey(functions.config().sendgrid.apikey);
 
-    const email = {
-        to: "chad@codingbychad.com",
-        from: "chad.e.smith@outlook.com",
-        subject: "CodingByChad Contact Form",
-        text: `${request.body.sender} sent: ${request.body.message}`,
-        html: `<h2>${request.body.sender}</h2><div><p>${request.body.message}</p></div>`
-    };
+        const email = {
+            to: "chad@codingbychad.com",
+            from: "chad.e.smith@outlook.com",
+            subject: "CodingByChad Contact Form",
+            text: `from: ${request.body.sender}, subject: ${request.body.subject} message: ${request.body.message}`,
+            html: `<h2>${request.body.sender}</h2>
+                <div><h4>${request.body.subject}</h4></div>
+                <div><p>${request.body.message}</p></div>`
+        };
 
-    try {
-        await sendgrid.send(email);
-        console.log('Message was sent!');
-        response.send(true);
-    } catch (error) {
-        console.log('Message failed!', request, email);
-        response.send(error);
+        try {
+            await sendgrid.send(email);
+            console.log('Message was sent!');
+            response.status(200).send(true);
+        } catch (error) {
+            console.log('Message failed!', request, email);
+            response.status(400).send(error);
+        }
+        response.end();
     }
-    response.end();
-})
+});

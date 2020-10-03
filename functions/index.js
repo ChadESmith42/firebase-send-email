3137e922b390fb53157671ff050305e17bdc63e6
Chad Smith <chad.smith@bluekc.com>
2020-10-03 00:34:01 -0500

    fix email issue refactor json for sendgrid requirements

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,17 @@
 const functions = require('firebase-functions');
 const sendgrid = require('@sendgrid/mail');
+const parser = require('body-parser');
 
-
-
-exports.contactUs = functions.https.onRequest(async (request, response) => {
+exports.contact_us = functions.https.onRequest(async (request, response) => {
 
     sendgrid.setApiKey(functions.config().sendgrid.apikey);
 
     const email = {
         to: "chad@codingbychad.com",
-        from: request.body.from,
+        from: "chad.e.smith@outlook.com",
         subject: "CodingByChad Contact Form",
-        text: request.body.text,
-        html: `<p>${request.body.text}</p>`
+        text: `${request.body.sender} sent: ${request.body.message}`,
+        html: `<h2>${request.body.sender}</h2><div><p>${request.body.message}</p></div>`
     };
 
     try {
@@ -20,7 +19,7 @@ exports.contactUs = functions.https.onRequest(async (request, response) => {
         console.log('Message was sent!');
         response.send(true);
     } catch (error) {
-        console.log('Message failed!', request, mailOptions);
+        console.log('Message failed!', request, email);
         response.send(error);
     }
     response.end();

d8ed2cc2ba97d51ff055ec95bdc8c5f607cb47a7
Chad Smith <chad.smith@bluekc.com>
2020-10-02 23:31:36 -0500

    integrate sendgrid api

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,22 @@
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
+const sendgrid = require('@sendgrid/mail');
 
-const email = functions.config().contactus.email;
-const password = functions.config().contactus.password;
 
-const mailTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: email,
-        pass: password
-    },
-});
 
 exports.contactUs = functions.https.onRequest(async (request, response) => {
-    const mailOptions = {
+
+    sendgrid.setApiKey(functions.config().sendgrid.apikey);
+
+    const email = {
+        to: "chad@codingbychad.com",
         from: request.body.from,
-        to: 'chad@codingbychad.com',
-        subject: '#CodingByChad Contact Form',
-        text: request.body.text
-    }
+        subject: "CodingByChad Contact Form",
+        text: request.body.text,
+        html: `<p>${request.body.text}</p>`
+    };
 
     try {
-        await mailTransport.sendMail(mailOptions);
+        await sendgrid.send(email);
         console.log('Message was sent!');
         response.send(true);
     } catch (error) {
